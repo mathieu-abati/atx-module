3936405c6d8294868edc8e2e28d4087fcba56ef7
Mathieu ABATI <mathieu.abati@gmail.com>
2025-11-10 17:34:07 +0100

    Script: HOLD_TIME is now useless

--- rpi_shutdown.py
+++ rpi_shutdown.py
@@ -4,14 +4,13 @@ from gpiozero import Button
 from subprocess import check_call
 from signal import pause
 
-HOLD_TIME = 2
 PIO_NB = 22
 
 def shutdown():
     print("Shutdown")
     check_call(['sudo', 'poweroff'])
 
-shutdown_btn = Button(PIO_NB, pull_up=None, active_state=True, hold_time=HOLD_TIME)
+shutdown_btn = Button(PIO_NB, pull_up=None, active_state=True)
 shutdown_btn.when_held = shutdown
 
 pause()
